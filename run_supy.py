f97550f036f088bad38c74bdaf439be0dbe75f68
Mat Lipson <m.lipson@unsw.edu.au>
2019-10-31 14:42:44 +0000

    redo commit removing outputs

---
+++ run_supy.py
@@ -0,0 +1,19 @@
+#%%
+import supy as sp
+from pathlib import Path
+
+
+#%%
+path_runcontrol = Path('./run_preston') / 'RunControl.nml'
+df_state_init = sp.init_supy(path_runcontrol)
+grid = df_state_init.index[0]
+df_forcing = sp.load_forcing_grid(path_runcontrol, grid)
+
+
+#%%
+df_output, df_state_final = sp.run_supy(
+            df_forcing, df_state_init,
+            save_state=False)
+
+
+df = df_output.loc[(98),'SUEWS']
